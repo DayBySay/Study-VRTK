0653ac4984c8d2f9f649fdb06cb666803ae4ea5e
Takayuki_Sei <takayuki_sei@voyagegroup.com>
2016-10-29 19:37:34 +0900

    攻撃を食らったときに音が鳴る
    すげー微妙だけどいったんこれで・・

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public AudioClip seDamage;
+	public AudioSource audio;
+	bool isInvincible = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	 void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "WeaponSwordEnemy")
+		{	
+			if (isInvincible) {
+				return;
+			}
+
+			damage();
+		}
+	}
+
+	void damage() {
+			audio.PlayOneShot(seDamage, 0.1f);
+			isInvincible = true;
+			Invoke("clearInvincible", 2.0f);
+	}
+
+	void clearInvincible() {
+		isInvincible = false;
+	}
+}
