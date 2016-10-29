33f962c3c42f27061535a06f9a46b91e32e82e41
Takayuki_Sei <takayuki_sei@voyagegroup.com>
2016-10-29 23:51:45 +0900

    剣を抜いた時の音！

---
+++ Assets/Scripts/TitleController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleController : VRTK.VRTK_InteractableObject {
+	bool starting = false;
+	public AudioSource audio;
+	public AudioClip seDrawn;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (starting)
+		{
+			return;
+		}
+
+		if (this.IsGrabbed())
+		{
+			drawn();
+		}
+	}
+
+	void drawn() {
+		audio.PlayOneShot(seDrawn, 1.0f);
+		starting = true;
+	}
+}
