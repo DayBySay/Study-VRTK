295cee0137260a0a2583e20a3af3c5b0aa458e34
Takayuki_Sei <takayuki_sei@voyagegroup.com>
2016-10-29 18:07:05 +0900

    音が鳴る

---
+++ Assets/Scripts/AudiorController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudiorController : MonoBehaviour {
+    public AudioClip se;
+
+	// Use this for initialization
+	void Start () {
+	        GetComponent<AudioSource>().PlayOneShot(se);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
