28ac2e99cfa35e061606e128bfe7181c71365e93
Takayuki_Sei <takayuki_sei@voyagegroup.com>
2016-10-29 13:18:29 +0900

    スケルトンが走って近づいてくるようになった

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+	[SerializeField, HideInInspector] NavMeshAgent agent;
+	[SerializeField, HideInInspector] Animator animator;
+
+	void Start()
+	{
+		agent = GetComponent<NavMeshAgent>();
+		animator = GetComponent<Animator>();
+	}
+
+
+	void Update () 
+	{
+		animator.SetFloat("Speed", agent.velocity.sqrMagnitude);
+	}
+}
