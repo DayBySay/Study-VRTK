c9d77fff8f56f4e566f5db7b39daccd59e97acb3
Takayuki_Sei <takayuki_sei@voyagegroup.com>
2016-10-29 14:14:38 +0900

    処理を自分のコントローラに移して手前でストップさせた

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,28 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+[RequireComponent(typeof (NavMeshAgent))]
+[RequireComponent(typeof (ThirdPersonCharacter))]
 
 public class EnemyController : MonoBehaviour {
 
 	[SerializeField, HideInInspector] NavMeshAgent agent;
 	[SerializeField, HideInInspector] Animator animator;
+	public ThirdPersonCharacter character { get; private set; }
+	public Transform target;
+	bool attackable;
 
 	void Start()
 	{
 		agent = GetComponent<NavMeshAgent>();
+		character = GetComponent<ThirdPersonCharacter>();
 		animator = GetComponent<Animator>();
+
+		agent.updateRotation = false;
+		agent.updatePosition = true;
 	}
 
-	void Update () 
-	{
+	 private void Update() 	{
 		animator.SetFloat("Speed", agent.velocity.magnitude);
+
+		 if (target != null)
+		 agent.SetDestination(target.position);
+
+		if (!attackable) {
+			character.Move(agent.desiredVelocity, false, false);
+		} else {
+			character.Move(Vector3.zero, false, false);
+			agent.Stop();
+		}
+	}
+
+
+	 public void SetTarget(Transform target) 
+	 {
+		 this.target = target;
 	}
 
 	 void OnTriggerEnter(Collider other) {
-		 Debug.Log(other);
-		
 		 	if (other.gameObject.tag == "Player") {
-				 agent.Stop();
+				 attackable = true;
 			 }
 	 }
-
 }
