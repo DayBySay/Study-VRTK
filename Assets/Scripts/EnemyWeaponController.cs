3111f7a30e69e07ccb911124d8db89a3e1156c74
Takayuki_Sei <takayuki_sei@voyagegroup.com>
2016-11-10 01:16:09 +0900

    敵の剣を弾けるようになった

---
+++ Assets/Scripts/EnemyWeaponController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyWeaponController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Shield") {
+			GameObject parent = transform.parent.parent.gameObject;
+			parent.GetComponent<Animator>().SetTrigger("Repelled");
+		}
+	}
+}
